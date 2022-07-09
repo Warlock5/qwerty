f563d60f886e4806097c2560d32facf5ab5100ce
warlock <all.exe.warlock@example.com>
2022-07-09 11:54:08 +0300

    Ex009_IntriArray

---
+++ Ex009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 11, 21, 31, 41, 51, 14, 24, 64, 65, 33, 45 };
+
+int result = Max(
+    Max(array[0], array[2], array[3]),
+    Max(array[4], array[5], array[6]),
+    Max(array[7], array[8], array[9])
+);
+System.Console.WriteLine(result);
+
+
