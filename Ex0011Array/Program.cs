49c62cc67de02b8824a9b5dfb8234ff86d8982bd
warlock <all.exe.warlock@example.com>
2022-07-09 11:53:25 +0300

    Ex0011Array

---
+++ Ex0011Array/Program.cs
@@ -0,0 +1,51 @@
+﻿
+void FillArray(int [] collection)
+{
+   int Length = collection.Length;
+   int index = 0;
+   while (index < Length) 
+   {
+    collection[index] = new Random().Next(1, 10);
+    index++;
+   }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count =col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] colltction, int find)
+{
+    int count = colltction.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(colltction[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+array[4] =4;
+array[6] =4;
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos = indexOf(array, 44);
+System.Console.WriteLine(pos);
